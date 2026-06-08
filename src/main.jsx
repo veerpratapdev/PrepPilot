f9376c9170fa9f416b0b7730f5a1b0a199adfdfe
Veer Pratap Dev <veerpratapdev75@gmail.com>
2026-06-08 14:18:52 +0530

    Fix GitHub Pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
 import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter basename="/PrepPilot">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
